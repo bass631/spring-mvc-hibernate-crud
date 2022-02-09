96735b67f199a76abd3db1231b35b3bb55e0fc23
bass631 <dima.bass@mail.ru>
2022-02-09 16:10:48 +0300

    2.3.1 v2

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -14,31 +14,31 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
         model.addAttribute("users", userService.getAllUsers());
         return "users/index";
     }
 
-    @RequestMapping("/new")
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
     public String addNewUser(Model model) {
         model.addAttribute("user", new User());
         return "users/new";
     }
 
-    @RequestMapping("/saveUser")
+    @RequestMapping(value = "/saveUser", method = RequestMethod.POST)
     public String saveUser(@ModelAttribute("user") User user) {
         userService.saveUser(user);
         return "redirect:/users/";
     }
 
-    @RequestMapping("/updateInfo")
+    @RequestMapping(value = "/updateInfo", method = RequestMethod.POST)
     public String updateUser(@RequestParam("userId") int id, Model model) {
         model.addAttribute("user", userService.getUser(id));
         return "users/new";
     }
 
-    @RequestMapping("/delete")
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public String deleteUser(@RequestParam("userId") int id) {
         userService.deleteUser(id);
         return "redirect:/users/";
